6f581108124a3e662f7cb344bce4cfb6f2139d1f
sakton <sakton@mail.ru>
2020-03-16 15:54:42 +0300

    JSON

--- NetModule/myjsonclientandserver.cpp
+++ NetModule/myjsonclientandserver.cpp
@@ -1,12 +1,112 @@
 #include "myjsonclientandserver.h"
-
-const char* NAME_USER = "nameUser";
-const char* ID_GAME = "idGame";
-const char* FIRE_TO_CELL = "fireCell";
-const char* MESSAGE = "message";
-const char* STATE_GAME = "stateGame";
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QJsonValue>
 
 MyJsonClientAndServer::MyJsonClientAndServer(const QByteArray &arr)
 {
+    createQByteArray();
+    *m_pByteArray = arr;
+    QJsonDocument doc = QJsonDocument::fromBinaryData(arr);
+    QJsonObject obj = doc.object();
+    m_idGame = obj[ID_GAME].toInt();
+    m_indexFireToCell = obj[INDEX_FIRE_TO_CELL].toInt();
+    m_stateEnemyUser = obj[STATE_ENEMY_USER].toInt();
+    m_resultEnemyFire = obj[RESULT_FIRE_USER].toInt();
+    m_message = obj[MESSAGE].toString();
+}
+
+MyJsonClientAndServer::MyJsonClientAndServer(int id, int idxFireToCell, int stateEnemyUser, int resultEnemyFire, const QString &mes)
+    : m_idGame{id},
+    m_indexFireToCell{idxFireToCell},
+    m_stateEnemyUser{stateEnemyUser},
+    m_resultEnemyFire{resultEnemyFire},
+    m_message{mes}
+{
 
 }
+
+MyJsonClientAndServer::~MyJsonClientAndServer()
+{
+    delete m_pByteArray;
+}
+
+MyJsonClientAndServer::MyJsonClientAndServer()
+    : m_idGame{-1},
+    m_indexFireToCell{-1},
+    m_stateEnemyUser{-1},
+    m_resultEnemyFire{-1},
+    m_message{""}
+{
+
+}
+
+QByteArray &MyJsonClientAndServer::toByteArray()
+{
+    QJsonDocument doc;
+    QJsonObject obj;
+    obj.insert(ID_GAME, m_idGame);
+    obj.insert(INDEX_FIRE_TO_CELL, m_indexFireToCell);
+    obj.insert(STATE_ENEMY_USER, m_stateEnemyUser);
+    obj.insert(RESULT_FIRE_USER, m_resultEnemyFire);
+    obj.insert(MESSAGE, m_message);
+    doc.setObject(obj);
+    *m_pByteArray = doc.toJson();
+    return *m_pByteArray;
+}
+
+int MyJsonClientAndServer::idGame() const
+{
+    return m_idGame;
+}
+
+void MyJsonClientAndServer::setIdGame(int idGame)
+{
+    m_idGame = idGame;
+}
+
+int MyJsonClientAndServer::indexFireToCell() const
+{
+    return m_indexFireToCell;
+}
+
+void MyJsonClientAndServer::setIndexFireToCell(int indexFireToCell)
+{
+    m_indexFireToCell = indexFireToCell;
+}
+
+int MyJsonClientAndServer::stateEnemyUser() const
+{
+    return m_stateEnemyUser;
+}
+
+void MyJsonClientAndServer::setStateEnemyUser(int stateEnemyUser)
+{
+    m_stateEnemyUser = stateEnemyUser;
+}
+
+int MyJsonClientAndServer::resultEnemyFire() const
+{
+    return m_resultEnemyFire;
+}
+
+void MyJsonClientAndServer::setResultEnemyFire(int resultEnemyFire)
+{
+    m_resultEnemyFire = resultEnemyFire;
+}
+
+QString MyJsonClientAndServer::message() const
+{
+    return m_message;
+}
+
+void MyJsonClientAndServer::setMessage(const QString &message)
+{
+    m_message = message;
+}
+
+void MyJsonClientAndServer::createQByteArray()
+{
+    m_pByteArray = new QByteArray;
+}
+
--- NetModule/myjsonclientandserver.h
+++ NetModule/myjsonclientandserver.h
@@ -7,20 +7,47 @@ class QByteArray;
 
 class MyJsonClientAndServer
 {
-    enum TypeGameJsonObject {
-
-    };
+    const char* ID_GAME = "idGame";
+    const char* INDEX_FIRE_TO_CELL = "indexFireCell";
+    const char* STATE_ENEMY_USER = "stateEnemyUser";
+    const char* RESULT_FIRE_USER = "stateOwnUser";
+    const char* MESSAGE = "message";
 
 public:
-    MyJsonClientAndServer(const QByteArray &arr);
+    MyJsonClientAndServer();
+    explicit MyJsonClientAndServer(const QByteArray &arr);
+    explicit MyJsonClientAndServer(int id, int idxFireToCell = -1, int stateEnemyUser = -1, int resultEnemyFire = -1, const QString &mes = "");
+    ~MyJsonClientAndServer();
+
+
+    QByteArray &toByteArray();
+
+    int idGame() const;
+    void setIdGame(int idGame);
+
+    int indexFireToCell() const;
+    void setIndexFireToCell(int indexFireToCell);
+
+    int stateEnemyUser() const;
+    void setStateEnemyUser(int stateEnemyUser);
+
+    int resultEnemyFire() const;
+    void setResultEnemyFire(int resultEnemyFire);
+
+    QString message() const;
+    void setMessage(const QString &message);
 
 private:
-    quint64 m_idGame;
-    int m_indexFireToCell;
-    QString message;
-    int stateMoveUser;
-    QString nameUser;
+    void createQByteArray();
 
+private:
+    QByteArray *m_pByteArray;
+    int m_idGame;
+    int m_indexFireToCell;
+    int m_stateEnemyUser;
+    int m_resultEnemyFire;
+    QString m_message;
+    //    QString m_nameUser;
 };
 
 #endif // MYJSONCLIENTANDSERVER_H
